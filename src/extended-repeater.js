c3880ed1eecd93e2fab1cedccf56f14beda26647
mafiti91 <ppivankov@gmail.com>
2022-05-02 23:17:26 +0300

    9.5

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let AddArr = []
+  let AddArr2 = []
+  let FirstStr = ""
+  let FinishStr = ""
+
+  if (str !== "") str = String(str);
+  if (options.addition !== "") options.addition = String(options.addition);
+  if (options.separator === "" || typeof options.separator === "undefined") options.separator = "+";
+  if (options.additionSeparator === "" || typeof options.additionSeparator === "undefined") options.additionSeparator = "|";
+
+  if (typeof options.additionRepeatTimes !== "undefined" && options.additionRepeatTimes > 0) {  
+      for (let i = 0; i < options.additionRepeatTimes; i++) { 
+        AddArr.push(options.addition)
+        AddArr.push(options.additionSeparator)  
+      }
+    AddArr.pop()
+    AddArr.unshift(str)
+    FirstStr = AddArr.join("")
+  } else {
+    AddArr.push(options.addition)
+    AddArr = AddArr.filter((el) => {
+      return el !== "undefined"
+    })
+    AddArr.unshift(str)
+    FirstStr = AddArr.join("") 
+  }
+
+  if (typeof options.repeatTimes !== "undefined" && options.repeatTimes > 0) {  
+    for (let i = 0; i < options.repeatTimes; i++) {  
+    AddArr2.push(FirstStr)
+    AddArr2.push(options.separator)
+    }
+  AddArr2.pop()
+  FirstStr = AddArr2.join("")
+}     
+
+return(FirstStr)
 }
 
 module.exports = {
